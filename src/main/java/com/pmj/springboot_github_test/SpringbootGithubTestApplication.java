21496ccdde520085914bb3c489fb15c340c9cf68
pmj888 <1712716086@qq.com>
2020-04-14 19:10:06 +0800

    测试团队开发

---
+++ src/main/java/com/pmj/springboot_github_test/SpringbootGithubTestApplication.java
@@ -0,0 +1,13 @@
+package com.pmj.springboot_github_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGithubTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pmj/springboot_github_test/SpringbootGithubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmj.springboot_github_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
